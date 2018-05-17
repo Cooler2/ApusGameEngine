0fac983e650dee8fef9c97ddcd95e32e47ccdc23
Cooler <ivan@apus-software.com>
2018-05-17 13:16:52 +0300

    Unfinished units in development
    --HG--
    branch : develop

---
+++ Painter3D8.pas
@@ -0,0 +1,198 @@
+// 3D drawing library.
+// Provides drawing of 3D-sprites, billboards, meshes etc.
+//
+// Copyright (C) 2003 Apus Software (www.games4win.com, www.apus-software.com)
+// Author: Ivan Polyacov (cooler@tut.by)
+unit Painter3D8;
+interface
+ uses DirectXGraphics,Geom3D,camera,EngineCls;
+
+type
+ T3DPainter8=class(T3DPainter)
+  constructor Create;
+  procedure Invalidate; override;
+  procedure SetCamera(c:TCamera); override;
+  procedure SetViewport(x1,y1,x2,y2:integer); override;
+  procedure DrawSprite(location:TMatrix43s;sprite:TTexture;
+                       doubleSide:boolean=false); override;
+  procedure GlobalTransp(alpha:single); virtual;
+ protected
+  camera:TCamera;
+  tex1,tex2:IDirect3dTexture8; // текущие текстуры на 1 и 2 стадиях
+  AlphaBlending,zWrite,zTest,lightning:boolean;
+  worldMat:boolean; // true если установлена не единичная матрица local->world
+  globalAlpha:cardinal;
+  cullmode:integer;
+ end;
+
+implementation
+ uses MyServis,d3d8,DxImages8;
+
+type
+ // Вершина в пространстве модели
+ MdlPoint=record
+  x,y,z:single;
+  nx,ny,nz:single;
+  color,specular:cardinal;
+  u,v:single;
+  u2,v2:single;
+ end;
+
+ // Вершина в пространстве камеры
+ ScrPoint=record
+  x,y,z,rhw:single;
+  color,specular:cardinal;
+  u,v:single;
+  u2,v2:single;
+ end;
+
+ // textured lit vertex
+ TVertexLT=record
+  x,y,z:single;
+  color,specular:cardinal;
+  u,v:single;
+ end;
+
+const
+ ShaderLT   = D3DFVF_XYZ+D3DFVF_DIFFUSE+D3DFVF_SPECULAR+D3DFVF_TEX1;
+ ShaderTLT  = D3DFVF_XYZRHW+D3DFVF_DIFFUSE+D3DFVF_SPECULAR+D3DFVF_TEX1;
+
+{ T3DPainter8 }
+
+constructor T3DPainter8.Create;
+begin
+ valid:=false;
+ ownTransform:=false;
+ globalAlpha:=$FF000000;
+ cullmode:=D3DCULL_CCW;
+end;
+
+procedure T3DPainter8.DrawSprite(location: TMatrix43s;
+  sprite: TTexture; doubleSide: boolean);
+var
+ vrts:array[0..3] of TVertexLT;
+begin
+ if not valid then Invalidate;
+
+ if OwnTransform then begin
+  // Using manual transformations
+  raise EWarning.Create('3DPainter: Not yet implemented');
+
+ end else begin
+  // Using D3D transformations
+{  with vrts[0] do begin
+   x:=0; y:=-1; z:=-1; color:=$FFFFFF+GlobalAlpha;
+   u:=spr.texsizeu*(0.5+spr.Startx);
+   v:=spr.texsizev*(0.5+spr.Starty+sprite.height);
+  end;
+  with vrts[1] do begin
+   x:=0; y:=-1; z:=1; color:=$FFFFFF+GlobalAlpha;
+   u:=spr.texsizeu*(0.5+spr.Startx);
+   v:=spr.texsizev*(0.5+spr.Starty);
+  end;
+  with vrts[2] do begin
+   x:=0; y:=1; z:=1; color:=$FFFFFF+GlobalAlpha;
+   u:=spr.texsizeu*(0.5+spr.Startx+spr.width);
+   v:=spr.texsizev*(0.5+spr.Starty);
+  end;
+  with vrts[3] do begin
+   x:=0; y:=1; z:=-1; color:=$FFFFFF+globalAlpha;
+   u:=spr.texsizeu*(0.5+spr.Startx+spr.width);
+   v:=spr.texsizev*(0.5+spr.Starty+spr.height);
+  end;
+  MultPnts(location,PPoint3s(@vrts),4,sizeof(TVertexLT));
+  if worldMat then begin
+   device.SetTransform(D3DTS_WORLD,D3DIdentMat);
+   worldMat:=false;
+  end;
+  device.SetVertexShader(ShaderLT);
+  if tex1<>spr.tex then begin
+   tex1:=spr.tex;
+   device.SetTexture(0,tex1);
+  end;
+  if lightning then begin
+   device.SetRenderState(D3DRS_LIGHTING,0);
+   lightning:=false;
+  end;
+  if DoubleSide then
+   device.SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE)
+  else
+   if CullMode<>D3DCULL_CCW then
+    device.SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+  device.DrawPrimitiveUP(D3DPT_TRIANGLEFAN,2,@vrts,sizeof(TVertexLT));}
+ end;
+end;
+
+procedure T3DPainter8.GlobalTransp(alpha: single);
+begin
+ globalAlpha:=round(255*alpha) shl 24;
+end;
+
+procedure T3DPainter8.Invalidate;
+var
+ bt:IDirect3DBaseTexture8;
+begin
+ worldMat:=true;
+ device.GetTexture(0,bt);
+ if (bt<>nil) and (bt.GetType=D3DRTYPE_TEXTURE) then
+  tex1:=IDirect3DTexture8(bt) else tex1:=nil;
+ device.GetTexture(1,bt);
+ if (bt<>nil) and (bt.GetType=D3DRTYPE_TEXTURE) then
+  tex2:=IDirect3DTexture8(bt) else tex2:=nil;
+end;
+
+procedure T3DPainter8.SetCamera(c: TCamera);
+var
+ m:TD3DMatrix;
+ cmv:TMatrix43v;
+ lu,lv,ln:double;
+begin
+ camera:=c;
+ move(c.trans,cmv,sizeof(cmv));
+ lu:=GetLength3(cmv[0]);
+ lv:=GetLength3(cmv[1]);
+ ln:=GetLength3(cmv[2]);
+ Normalize3(cmv[0]);
+ Normalize3(cmv[1]);
+ Normalize3(cmv[2]);
+ // Right direction
+ m.m[0,0]:=cmv[0].x;
+ m.m[1,0]:=cmv[0].y;
+ m.m[2,0]:=cmv[0].z;
+ // Up direction
+ m.m[0,1]:=-cmv[1].x;
+ m.m[1,1]:=-cmv[1].y;
+ m.m[2,1]:=-cmv[1].z;
+ // View direction
+ m.m[0,2]:=cmv[2].x;
+ m.m[1,2]:=cmv[2].y;
+ m.m[2,2]:=cmv[2].z;
+ // Position
+ m.m[3,0]:=-DotProduct3(cmv[0],cmv[3]);
+ m.m[3,1]:=DotProduct3(cmv[1],cmv[3]);
+ m.m[3,2]:=-DotProduct3(cmv[2],cmv[3]);
+ // Other
+ m.m[0,3]:=0;  m.m[1,3]:=0;  m.m[2,3]:=0;  m.m[3,3]:=1;
+ device.SetTransform(D3DTS_VIEW,m);
+
+ fillchar(m,sizeof(m),0);
+ m.m[0,0]:=ln/lu;
+ m.m[1,1]:=ln/lv;
+ m.m[2,2]:=c.FarDist/(c.fardist-c.neardist);
+ m.m[2,3]:=1;
+ m.m[3,2]:=-m.m[2,2]*c.neardist;
+ device.SetTransform(D3DTS_PROJECTION,m);
+end;
+
+procedure T3DPainter8.SetViewport(x1, y1, x2, y2: integer);
+var
+ vp:TD3DViewport8;
+begin
+ vp.X:=x1; vp.Y:=x1;
+ vp.Width:=x2-x1+1;
+ vp.Height:=y2-y1+1;
+ vp.MinZ:=0; vp.MaxZ:=1;
+ device.SetViewport(vp);
+end;
+
+end.
---
+++ videoCapture.pas
@@ -0,0 +1,337 @@
+unit VideoCapture;
+interface
+ uses EngineCls,BasicGame,Images;
+
+ // Инициализирует захват видео, стартует потоки и т.д.
+ function StartVideoCapture(game:TBasicGame;outfile:string;options:cardinal=0;threads:integer=2):boolean;
+ // Видеозахват завершен (однако упаковка и запись могут еще продолжаться)
+ procedure FinishVideoCapture;
+ // Используется для передачи кадра на сжатие
+ procedure StoreFrame(img:TRAWImage);
+
+implementation
+ uses windows,SysUtils,MyServis,classes,AVS,gfxformats;
+ const
+  MAX_FRAMES = 50000;
+ type
+
+  // Главный поток - принимает входящие кадры, генерирует задания 
+  TVideoThread=class(TThread)
+   procedure Execute; override;
+  end;
+
+  // Рабочий поток - исполняет задания из очереди
+  TWorkerThread=class(TThread)
+   procedure Execute; override;
+  end;
+
+  // Поток записывает готовые данные в файл
+  TWriterThread=class(TThread)
+   procedure Execute; override;
+  end;
+
+  // Кадр для сжатия
+  TFrameData=class
+   source:TRAWImage; // исходные RAW-данные в 32bpp
+   timestamp:cardinal; // время захвата кадра в мс (со старта видеопотока)
+   frameType:cardinal;
+   frameNum:integer;
+   chunks:array[1..15] of ByteArray;
+   chunksCount:integer;
+   chunksFinished:integer; 
+   next,prev:TFrameData;
+   constructor Create(raw:TRAWImage);
+   destructor Destroy; override;
+   procedure FreeSource;
+  end;
+
+  // Задание
+  TJob=record
+   frame:TFrameData; // Какой кадр обрабатывать
+   rect:TRect;   // область кадра для обработки
+   task:cardinal; // что делать
+   chunk:integer; // в какой чанк писать результат
+  end;
+
+ var
+  videoThread:TVideoThread;
+  workers:array[1..8] of TWorkerThread;
+  gameObj:TBasicGame;
+  // Кадры для обработки
+  firstFrame,lastFrame:TFrameData;
+  unhandledFrame:TFrameData; // следующий для обработки кадр
+  dFrames:integer; // кол-во последовательных D-кадров
+
+  // Очередь заданий
+  jobQueue:array[0..255] of TJob;
+  jStart,jEnd:byte;
+  jCount:integer; // кол-во заданий в очереди
+
+  cSect:TMyCriticalSection;
+  capStarted:int64;
+  filename:string; // имя файла для записи результата
+  captureMode:boolean=false;
+  readyState:boolean=true;
+
+  // Готовые кадры после обработки
+  fCount:integer;
+
+ function StartVideoCapture(game:TBasicGame;outfile:string;options:cardinal=0;threads:integer=2):boolean;
+  var
+   i:integer;
+  begin
+   result:=false;
+   if not readyState then exit;
+   LogMessage('Starting video capture to '+outfile);
+   EnterCriticalSection(cSect);
+   try
+    // Free any old frame data if exists
+    while firstFrame<>nil do firstFrame.Free;
+    unhandledFrame:=nil;
+    readyState:=false;
+    captureMode:=true;
+    gameObj:=game;
+    capStarted:=MyTickCount;
+    fCount:=0;
+    dFrames:=0;
+    jCount:=0;
+    filename:=outfile;
+    videoThread:=TVideoThread.Create(false);
+    for i:=1 to threads do
+     workers[i]:=TWorkerThread.Create(false);
+    TWriterThread.Create(false); 
+    result:=true;
+   finally
+    LeaveCriticalSection(cSect);
+   end;
+  end;
+
+ procedure FinishVideoCapture;
+  begin
+   LogMessage('Finishing video capture');
+   captureMode:=false;
+   videoThread.Terminate;
+  end;
+
+ procedure StoreFrame(img:TRAWImage);
+  var
+   frame:TFrameData;
+   fType:cardinal;
+  begin
+   EnterCriticalSection(cSect);
+   try
+    frame:=TFrameData.Create(img);
+    if unhandledFrame=nil then unhandledFrame:=frame;
+   finally
+    LeaveCriticalSection(cSect);
+   end;
+  end;
+
+{ TVideoThread }
+
+ procedure AddJob(var job:TJob);
+  begin
+   while jCount>250 do sleep(5);
+   EnterCriticalSection(cSect);
+   try
+    jobQueue[jEnd]:=job;
+    inc(jEnd);
+    inc(jCount);
+   finally
+    LeaveCriticalSection(cSect);
+   end;
+  end;
+
+ // разбирается с кадром и создает задания 
+ procedure ProcessFrame(frame,oldFrame:TFrameData);
+  var
+   job:TJob;
+  begin
+   frame.frameType:=1;
+{   if (oldFrame<>nil) and (dFrames<50) then
+    if CompareFrames(oldFrame.source,Frame.source) then frame.frameType:=2;}
+
+   // process P-Frame
+   if frame.frameType=1 then begin
+    frame.chunksCount:=1;
+    job.frame:=frame;
+    job.task:=1;
+    job.chunk:=1;
+    job.rect:=Rect(0,0,frame.source.width,frame.source.height);
+    AddJob(job);
+   end;
+
+   // Process D-Frame
+   if frame.frameType=2 then begin
+    frame.chunksCount:=1;
+    job.frame:=frame;
+    job.task:=2;
+    job.chunk:=1;
+    job.rect:=Rect(0,0,frame.source.width,frame.source.height);
+    AddJob(job);
+   end;
+  end;
+
+ procedure TVideoThread.Execute;
+  var
+   frame,oldFrame:TFrameData;
+  begin
+   repeat // Главный цикл
+    EnterCriticalSection(cSect);
+    try
+     // Есть ли необработанный кадр?
+     frame:=nil;
+     if unhandledFrame<>nil then begin
+      frame:=unhandledFrame;
+      oldFrame:=unhandledFrame.prev;
+      unhandledFrame:=frame.next;
+     end;
+    finally
+     LeaveCriticalSection(cSect);
+    end;
+    if frame<>nil then ProcessFrame(frame,oldFrame)
+     else sleep(5);
+   until terminated;  
+   FreeAndNil(videoThread);
+  end;
+
+{ TWorkerThread }
+procedure DoJob(var job:TJob);
+ var
+  t:integer;
+ begin
+  try
+  with job do begin
+   t:=task and $FF;
+   case t of
+    1:PackPFrame(frame.source,rect,frame.chunks[chunk],AVS_RAW18);
+ //   2:PackPFrame(
+   end;
+   InterlockedIncrement(frame.chunksFinished);
+  end;
+  except
+   on e:exception do LogMessage('Error processing job: '+IntToHex(cardinal(job.frame),8)+
+     ' Frame# '+IntToStr(job.frame.frameNum)+
+     ' Chunk: '+IntToStr(job.chunk)+' task: '+IntToHex(job.task,8));
+  end;
+ end;
+
+procedure TWorkerThread.Execute;
+ var
+  job:TJob;
+ begin
+  freeOnTerminate:=true;
+  repeat
+   job.task:=0;
+   EnterCriticalSection(cSect);
+   try
+    if jStart<>jEnd then begin
+     job:=jobQueue[jStart];
+     inc(jStart);
+     dec(jCount);
+    end else
+     if videoThread=nil then Terminate; // everything done
+   finally
+    LeaveCriticalSection(cSect);
+   end;
+   if job.task<>0 then DoJob(job)
+    else sleep(1);
+  until terminated;
+ end;
+
+ { TFrameData }
+constructor TFrameData.Create;
+ begin
+  source:=raw;
+  timeStamp:=MyTickCount-capStarted;
+  next:=nil;
+  prev:=lastFrame;
+  if lastFrame<>nil then lastFrame.next:=self;
+  lastFrame:=self;
+  if firstFrame=nil then firstFrame:=self;
+  chunksCount:=0;
+  chunksFinished:=0;
+  inc(fCount);
+  frameNum:=fCount;
+ end;
+
+destructor TFrameData.Destroy;
+ var
+  i:integer;
+ begin
+  EnterCriticalSection(cSect);
+  try
+  FreeSource;
+  for i:=1 to chunksCount do
+   SetLength(chunks[i],0);
+  chunksCount:=0;
+  if firstFrame=self then firstFrame:=next;
+  if lastFrame=self then lastFrame:=nil;
+  inherited;
+  finally
+   LeaveCriticalSection(cSect);
+  end;
+ end;
+
+procedure TFrameData.FreeSource;
+ begin
+  if source<>nil then gameObj.ReleaseFrameData(source);
+  source:=nil;
+ end;
+
+{ TWriterThread }
+
+procedure TWriterThread.Execute;
+var
+ f:file;
+ frame:TFrameData;
+ i:integer;
+ header:TFrameHeader;
+ data:ByteArray;
+begin
+ FreeOnTerminate:=true;
+ assign(f,filename);
+ rewrite(f,1);
+ repeat
+  frame:=nil;
+  EnterCriticalSection(cSect);
+  try
+   if (firstFrame<>nil) and
+      (firstFrame.chunksCount>0) and
+      (firstFrame.chunksCount=firstFrame.chunksFinished) then frame:=firstFrame;
+  finally
+   LeaveCriticalSection(cSect);
+  end;
+  if frame<>nil then begin
+   with frame do begin
+    // Покадровая запись в файлы
+    data:=SaveTGA(frame.source);
+    WriteFile(filename+IntToStr(frame.frameNum)+'.tga',@data[0],0,length(data));
+    SetLength(data,0);
+
+{    // Frame header
+    header.magic:=MAGIC_DWORD;
+    header.timestamp:=timestamp;
+    header.frameType:=frameType;
+    header.frameNum:=frameNum;
+    header.frameSize:=sizeof(header);
+    for i:=1 to chunksCount do
+     inc(header.frameSize,length(chunks[i]));
+    BlockWrite(f,header,sizeof(header));
+    // Chunks
+    for i:=1 to chunksCount do
+     BlockWrite(f,chunks[i][0],length(chunks[i]));      }
+    frame.Free; // передвигает указатель firstFrame на след. кадр 
+   end;
+  end else
+   if videoThread=nil then break;
+ until false;
+ close(f);
+ readyState:=true;
+end;
+
+initialization
+ InitCritSect(cSect,'videoCap');
+finalization
+ DeleteCritSect(cSect);
+end.
