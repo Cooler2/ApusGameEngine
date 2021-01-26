95f99557ecea448a20d47c452a8955554f561290
Cooler <ivan@apus-software.com>
2021-01-26 15:05:54 +0300

    SteamAPI: platform-dependent library names

--- Apus.Engine.SteamAPI.pas
+++ Apus.Engine.SteamAPI.pas
@@ -6,10 +6,16 @@
 unit Apus.Engine.SteamAPI;
 interface
  const
-  {$IFDEF CPU64}
-  steamLibName = 'steam_api64.dll';
-  {$ELSE}
-  steamLibName = 'steam_api.dll';
+  {$IFDEF MSWINDOWS}
+   {$IFDEF CPU64}
+   steamLibName = 'steam_api64.dll';
+   {$ELSE}
+   steamLibName = 'steam_api.dll';
+   {$ENDIF}
+  {$ENDIF}
+  {$IFDEF LINUX}
+   //{$LINKLIB steam_api}
+   steamLibName = 'libsteam_api.so';
   {$ENDIF}
  var
   steamAvailable:boolean=false;   // Is API available?
