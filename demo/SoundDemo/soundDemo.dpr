6361eb1a899c21e41c7e4a42d9c7a1812ad460ce
Cooler <ivan@apus-software.com>
2021-03-01 01:38:28 +0300

    SoundDemo: demo project for the sound system

---
+++ demo/SoundDemo/soundDemo.dpr
@@ -0,0 +1,65 @@
+{$APPTYPE CONSOLE}
+
+program soundDemo;
+
+uses
+  SysUtils,
+  Apus.MyServis,
+  Apus.EventMan,
+  Apus.Engine.API,
+  Apus.Engine.Sound;
+
+const
+ defaultCmd:array[1..10] of string=(
+  'PlayMusic\testOGG',
+  'PlayMusic\testMP3',
+  'PlayMusic\testMOD::3',
+  'PlayMusic\None',
+  'Play\Sample',
+  'Play\Stereo',
+  'Play\Low',
+  'Play\Wav',
+  'Play\sample',
+  'Play\sample');
+
+
+var
+ cmd:string;
+ v:integer;
+ tag:int64;
+begin
+ try
+  UseLogFile('test.log',true);
+  soundFolderPath:='Res\';
+  writeln('Select backend library: ');
+  writeln(' 1 - !Mixer (ImxEx.dll)');
+  writeln(' 2 - BASS (bass.dll)');
+  writeln(' 3 - SDL Mixer (sdl_mixer.dll)');
+  readln(cmd);
+  case ParseInt(cmd) of
+   1:InitSoundSystem(slIMixer);
+   2:InitSoundSystem(slBass);
+   3:InitSoundSystem(slSDL);
+  end;
+
+  writeln('Enter command (like "Play\sample") or '#13#10'1..',high(defaultCmd),' for predefined commands or "q" to exit.');
+
+  repeat
+   write('Sound\:');
+   readln(cmd);
+   if length(cmd)<3 then begin
+    v:=ParseInt(cmd);
+    if (v>0) and (v<=high(defaultCmd)) then begin
+     cmd:=defaultCmd[v];
+     writeln('SOUND\'+cmd);
+    end;
+   end;
+   if SameText(cmd,'q') then break;
+   Signal('SOUND\'+cmd,tag);
+  until false;
+
+  DoneSoundSystem;
+ except
+  on E:Exception do writeln('Error: '+ExceptionMsg(e));
+ end;
+end.
---
+++ demo/SoundDemo/sounds.ctl
@@ -0,0 +1,44 @@
+# ---------------------------------------------- #
+#        Sounds and music definitions            #
+#                                                #
+# „тобы исключить какой-нить звук, следует       #
+# поставить ему значение none. ћожно назначить   #
+# несколько звуков на одно событие использу€     #
+# синтаксис задани€ массивов. “огда звук будет   #
+# равноверо€тно выбиратьс€ из заданных вариантов #
+# ---------------------------------------------- #
+
+$Section Settings
+  ; какие сэмплы загружать предварительно
+  PreloadSamples        "sample.ogg"
+$EndOfSection
+
+
+$Section Music
+  $Section TestOGG
+    File     "music.ogg"
+    volume   50
+  $EndOfSection
+
+  $Section TestMP3
+    File     "musicLow.mp3"
+    volume   50
+    loop     OFF
+  $EndOfSection
+
+  $Section TestMOD
+    File     "x-tag.mod"
+    volume   50
+  $EndOfSection
+
+$EndOfSection
+
+
+$Section SoundEvents
+  ; Format: EventName  "filename,vol=xx,pan=yy"
+  Sample    "sample.ogg"
+  Low       "sampleLow.ogg"
+  Stereo    "sampleStereo.ogg"
+  Wav       "sampleWav.wav"
+
+$EndOfSection
