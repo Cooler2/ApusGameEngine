ff3056383f4bbf8fbb750d4c8bb17eab76ad98f4
Cooler <ivan@apus-software.com>
2021-12-03 17:40:48 +0300

    IMixEx: don't allow to compile on non-win32 platform

--- Base/extra/IMixEx.pas
+++ Base/extra/IMixEx.pas
@@ -2,7 +2,9 @@
 // Installer (dll + lib + documentation): http://apus-software.com/imixer/bin/IMixerPro.exe
 // Don't confuse IMix* (IMixer) and IMX* (IMixerPro) - they are different things.
 unit IMixEx;
-
+{$IF not Defined(MSWINDOWS) or Defined(CPU64) or Defined(CPUx64)}
+ {$FATAL 'IMixer is only available on Win32'}
+{$ENDIF}
 interface
 uses windows;
 type
@@ -61,65 +63,65 @@ const
  IMX_3DALG_LIGHT     = 3;
 
  //      Initialization done
- function IMXInit(hwnd:HWnd;freq,flags:DWORD;reserved:integer=-1):boolean; cdecl; external 'IMxEx.dll' delayed;
- function IMXUninit:longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXStart:longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXStop:longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXPause(Pause:longbool):longbool; cdecl; external 'IMxEx.dll' delayed;
+ function IMXInit(hwnd:HWnd;freq,flags:DWORD;reserved:integer=-1):boolean; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXUninit:longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXStart:longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXStop:longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXPause(Pause:longbool):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
 
  //      Control mixer
- function IMXSetGlobalVolumes(modvol,smpvol,streamvol:integer):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXGetGlobalVolumes(var modvol,smpvol,streamvol:integer):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXSetBlockSize(sec:double):double; cdecl; external 'IMxEx.dll' delayed;
- function IMXGetVersion:cardinal; cdecl; external 'IMxEx.dll' delayed;
+ function IMXSetGlobalVolumes(modvol,smpvol,streamvol:integer):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXGetGlobalVolumes(var modvol,smpvol,streamvol:integer):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXSetBlockSize(sec:double):double; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXGetVersion:cardinal; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
 
  //      3D functions
- function IMXCommit3D:longbool; cdecl; external 'IMxEx.dll' delayed;
+ function IMXCommit3D:longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXSetListnerPosition(Pos:PIMX3DVector=nil;vel:PIMX3DVector=nil;
-            front:PIMX3DVector=nil;top:PIMX3DVector=nil):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXSetEnvironmentFactors(distance:single=-1;rolloff:single=-1;doppler:single=-1):longbool; cdecl; external 'IMxEx.dll' delayed;
- procedure IMXSet3DAlgorithm(Algorithm:cardinal=IMX_3DALG_DEFAULT); cdecl; external 'IMxEx.dll' delayed;
+            front:PIMX3DVector=nil;top:PIMX3DVector=nil):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXSetEnvironmentFactors(distance:single=-1;rolloff:single=-1;doppler:single=-1):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ procedure IMXSet3DAlgorithm(Algorithm:cardinal=IMX_3DALG_DEFAULT); cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
 
  //      Sample
  function IMXSampleLoad(FromMem:longbool;FileName:PAnsiChar;Offset:DWORD=0;
-                        Length:DWord=0;res2:DWORD=0;Flags:DWORD=0):HSample; cdecl; external 'IMxEx.dll' delayed;
+                        Length:DWord=0;res2:DWORD=0;Flags:DWORD=0):HSample; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXSamplePlay(hsmp:HSAMPLE;vol:integer=100;pan:integer=0;
-                        freq:DWORD=0;start:DWORD=0):HChannel; cdecl; external 'IMxEx.dll' delayed;
+                        freq:DWORD=0;start:DWORD=0):HChannel; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXSamplePlay3D(hsmp:HSAMPLE;pos:PIMX3DVector=nil;orient:PIMX3DVector=nil;
-            vel:PIMX3DVector=nil;vol:integer=100;freq:cardinal=0;start:cardinal=0):HChannel; cdecl; external 'IMxEx.dll' delayed;
+            vel:PIMX3DVector=nil;vol:integer=100;freq:cardinal=0;start:cardinal=0):HChannel; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXSampleSet3DParameters(hsmp:HSAMPLE;IAngle:integer=-1;OAngle:integer=-1;outVol:integer=-1;
-            MinDist:single=-1;MaxDist:single=-1;Mode:integer=-1):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXSampleUnload(hsmp:HSample):longbool; cdecl; external 'IMxEx.dll' delayed;
+            MinDist:single=-1;MaxDist:single=-1;Mode:integer=-1):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXSampleUnload(hsmp:HSample):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
 
  //      Module
  function IMXModuleLoad(FromMem:longbool;FileName:PAnsiChar;Offset:DWORD=0;
-                        Length:DWORD=0;res2:DWORD=0):HMod; cdecl; external 'IMxEx.dll' delayed;
- function IMXModulePlay(hmod:HMod;flags:integer=-1):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXModuleUnload(hmod:HMod):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXModuleAttachInstruments(targethmod,sourcehmod:HMod):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXModuleAdjustBPM(hmod:HMod;bpm:double=1):longbool; cdecl; external 'IMxEx.dll' delayed;
+                        Length:DWORD=0;res2:DWORD=0):HMod; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXModulePlay(hmod:HMod;flags:integer=-1):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXModuleUnload(hmod:HMod):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXModuleAttachInstruments(targethmod,sourcehmod:HMod):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXModuleAdjustBPM(hmod:HMod;bpm:double=1):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
 
  //      Stream
  function IMXStreamOpenFile(fromMem:longbool;filename:PAnsiChar;offset:DWORD=0;
-            length:DWORD=0;flags:DWORD=0):HStream; cdecl; external 'IMxEx.dll' delayed;
- function IMXStreamPlay(hstream:HStream;flags:integer=-1):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXStreamClose(hstream:HStream):longbool; cdecl; external 'IMxEx.dll' delayed;
+            length:DWORD=0;flags:DWORD=0):HStream; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXStreamPlay(hstream:HStream;flags:integer=-1):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXStreamClose(hstream:HStream):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
 
  //      Channel
- function IMXChannelStop(hchan:HChannel):longbool; cdecl; external 'IMxEx.dll' delayed;
+ function IMXChannelStop(hchan:HChannel):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXChannelSetAttributes(hchan:HChannel;vol:integer=-1;pan:integer=-101;
-                                  freq:integer=-1):longbool; cdecl; external 'IMxEx.dll' delayed;
+                                  freq:integer=-1):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXChannelSetSync(hchan:HChannel;sType,param:DWORD;proc:SyncProc;
-                            User:DWORD):HSync; cdecl; external 'IMxEx.dll' delayed;
+                            User:DWORD):HSync; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXChannelSlide(hchan:HChannel;vol:integer=-1;pan:integer=-101;freq:DWORD=0;
-                          time:integer=100):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXChannelSetPosition(hchan:HChannel;pos:DWORD):longbool; cdecl; external 'IMxEx.dll' delayed;
- function IMXChannelRemoveSync(hchan:HChannel;sync:HSync):longbool; cdecl; external 'IMxEx.dll' delayed;
+                          time:integer=100):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXChannelSetPosition(hchan:HChannel;pos:DWORD):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
+ function IMXChannelRemoveSync(hchan:HChannel;sync:HSync):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXChannelSet3DParameters(hchan:HChannel;IAngle:integer=-1;OAngle:integer=-1;
             outVol:integer=-1;MinDist:single=-1;MaxDist:single=-1;
-            Mode:integer=-1):longbool; cdecl; external 'IMxEx.dll' delayed;
+            Mode:integer=-1):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
  function IMXChannelSet3DPosition(hchan:HChannel;pos:PIMX3DVector=nil;orient:PIMX3DVector=nil;
-                                  vel:PIMX3DVector=nil):longbool; cdecl; external 'IMxEx.dll' delayed;
+                                  vel:PIMX3DVector=nil):longbool; cdecl; external 'IMxEx.dll' {$IFNDEF FPC} delayed {$ENDIF};
 
 implementation
 
