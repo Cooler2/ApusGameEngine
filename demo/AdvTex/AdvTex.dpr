3169eae33072c3159632bbe1dfe838abf376e63a
Cooler <ivan@apus-software.com>
2022-01-08 23:32:03 +0300

    AdvTex demo update

--- demo/AdvTex/AdvTex.dpr
+++ demo/AdvTex/AdvTex.dpr
@@ -2,7 +2,7 @@
 // - Texture Array
 // - Manual Mip-Map levels
 program AdvTex;
- uses Apus.MyServis, SysUtils, Types, Apus.EventMan, Apus.Engine.API,
+ uses Apus.MyServis, SysUtils, Types, Apus.Colors, Apus.EventMan, Apus.Engine.API,
    Apus.Engine.GameApp, Apus.Engine.UIScene, Apus.Engine.UIClasses,
    Apus.FastGFX, Apus.Engine.ResManGL;
 
@@ -42,21 +42,22 @@ procedure TMainScene.Initialize;
   DrawToTexture(mipTex,0);
   for y:=0 to 127 do
    for x:=0 to 127 do
-    PutPixel(x,y,$FF203040+x+$C00000*byte(y and 15<2)+$A000*byte(x and 15<2));
+    PutPixel(x,y,MyColor($C0*byte(y and 15<2),$A0*byte(x and 15<2),x*2));
   mipTex.Unlock;
   DrawToTexture(mipTex,1);
   for y:=0 to 63 do
    for x:=0 to 63 do
-    PutPixel(x,y,$FF101010+y*2+$C00000*byte(y and 7<1)+$A000*byte(x and 7<1));
+    PutPixel(x,y,MyColor($C0*byte(y and 7<1),$A0*byte(x and 7<1),0));
   mipTex.Unlock;
 
-  arrTex:=resourceManagerGL.AllocArray(128,128,TImagePixelFormat.ipfARGB,4,0,'arrTex');
+  // 4 layers texture array
+  arrTex:=resourceManagerGL.AllocArray(128,128,TImagePixelFormat.ipfARGB,4,aiMipMapping,'arrTex');
   for z:=0 to 3 do begin
    arrTex.Lock(z);
    SetRenderTarget(arrTex.data,arrTex.pitch,128,128);
    for y:=0 to 127 do
     for x:=0 to 127 do
-     PutPixel(x,y,$FF203040+x*5+(y) shl 8+(z*70) shl 16);
+     PutPixel(x,y,MyColor(z*70,(x xor y)*2,(x xor y))); // same texture with different RED channel for each layer
    arrTex.Unlock;
   end;
   arrShader:=shader.Load('res\arrShader');
@@ -69,25 +70,31 @@ procedure TMainScene.Render;
  begin
   gfx.target.Clear($FF000000);
   scale:=0.9+0.6*sin(MyTickCount/1000);
-  mipTex.SetFilter(fltNearest);
+  mipTex.SetFilter(TTexFilter.fltNearest);
   draw.Scaled(120,50,0.4,mipTex);
   draw.Scaled(120,150,1,mipTex);
   draw.Scaled(120,320,1.6,mipTex);
   draw.Scaled(120,550,scale,mipTex);
-  mipTex.SetFilter(fltBilinear);
+  txt.WriteW(game.defaultFont,120,720,$FFFFFFFF,'Nearest',taCenter);
+  mipTex.SetFilter(TTexFilter.fltBilinear);
   draw.Scaled(340,50,0.4,mipTex);
   draw.Scaled(340,150,1,mipTex);
   draw.Scaled(340,320,1.6,mipTex);
   draw.Scaled(340,550,scale,mipTex);
-  mipTex.SetFilter(fltTrilinear);
+  txt.WriteW(game.defaultFont,340,720,$FFFFFFFF,'Bilinear',taCenter);
+  mipTex.SetFilter(TTexFilter.fltTrilinear);
   draw.Scaled(580,50,0.4,mipTex);
   draw.Scaled(580,150,1,mipTex);
   draw.Scaled(580,320,1.6,mipTex);
   draw.Scaled(580,550,scale,mipTex);
+  txt.WriteW(game.defaultFont,580,720,$FFFFFFFF,'Trilinear',taCenter);
+
 
   // Texture array
   shader.UseCustom(arrShader);
   draw.Image(800,20,arrTex);
+  draw.Scaled(860,220,0.6,arrTex);
+  draw.Scaled(860,300,0.3,arrTex);
   shader.Reset;
  end;
 
--- demo/AdvTex/res/arrShader.glsl
+++ demo/AdvTex/res/arrShader.glsl
@@ -1,4 +1,7 @@
 #version 330
+
+[VERTEX]
+
 uniform mat4 MVP;
 layout (location=0) in vec3 position;
 layout (location=1) in vec4 color;
@@ -11,4 +14,18 @@ void main(void)
    gl_Position = MVP*vec4(position,1.0);
    vColor = color;
    vTexCoord = texCoord;
+}
+
+[FRAGMENT]
+
+uniform sampler2DArray tex0;
+in vec4 vColor;
+in vec2 vTexCoord;
+out vec4 fragColor;
+
+void main(void)
+{
+  vec3 c = vColor.bgr;
+  vec4 col = texture(tex0, vec3(vTexCoord, vTexCoord.x+vTexCoord.y*2));
+  fragColor = col;
 }
