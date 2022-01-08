39812ba8be54c759db965259b24f3dae9edc4a77
Cooler <ivan@apus-software.com>
2022-01-08 22:17:46 +0300

    Types: code formatting

--- Base/Apus.Types.pas
+++ Base/Apus.Types.pas
@@ -3,39 +3,41 @@ interface
 
 type
   // 8-bit string type (assuming UTF-8 encoding)
-  Char8=UTF8Char;
-  String8=UTF8String;
-  PString8=^String8;
+  Char8 = UTF8Char;
+  String8 = UTF8String;
+  PString8 = ^String8;
   // 16-bit string type (can be UTF-16 or UCS-2)
   {$IFDEF UNICODE}
-  Char16=Char;
-  String16=UnicodeString;
+  Char16 = Char;
+  String16 = UnicodeString;
   {$ELSE}
-  char16=WideChar;
-  String16=WideString;
+  char16 = WideChar;
+  String16 = WideString;
   {$ENDIF}
-  PString16=^String16;
+  PString16 = ^String16;
+
+  String32 = UCS4String;
 
   // String arrays
-  StringArray8=array of String8;
-  StringArray16=array of String16;
-  StringArray=array of string; // depends on UNICODE mode
+  StringArray8 = array of String8;
+  StringArray16 = array of String16;
+  StringArray = array of string; // depends on UNICODE mode
 
   {$IF Declared(TBytes)}
-  ByteArray=TBytes;
+  ByteArray = TBytes;
   {$ELSE}
-  ByteArray=array of byte;
+  ByteArray = array of byte;
   {$ENDIF}
-  WordArray=array of word;
-  IntArray=array of integer;
-  UIntArray=array of cardinal;
-  SingleArray=array of single;
-  FloatArray=array of double;
-  ShortStr=string[31];
-  PointerArray=array of pointer;
-  VariantArray=array of variant;
+  WordArray = array of word;
+  IntArray = array of integer;
+  UIntArray = array of cardinal;
+  SingleArray = array of single;
+  FloatArray = array of double;
+  ShortStr = string[31];
+  PointerArray = array of pointer;
+  VariantArray = array of variant;
 
-  TProcedure=procedure;
+  TProcedure = procedure;
 
 implementation
 
