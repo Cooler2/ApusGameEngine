312762fd19dc7e486f3fe408f55c0a3086acb95c
Cooler <ivan@apus-software.com>
2022-05-26 00:25:03 +0300

    Engine.Types: color gradients

--- Apus.Engine.Types.pas
+++ Apus.Engine.Types.pas
@@ -50,7 +50,50 @@ type
 
  TFontHandle=cardinal;
 
+ TMonoGradient=record
+  base,dx,dy:single;
+  procedure Init(v1,v2,angle,scale:single);
+  function ValueAt(x,y:single):single; inline;
+ end;
+ // Linear gradient
+ TColorGradient=record
+  red,green,blue,alpha:TMonoGradient;
+  procedure Init(color1,color2:cardinal;angle,scale:single);
+  function ColorAt(x,y:single):cardinal;
+ end;
 
 implementation
+ uses Apus.MyServis;
+ {$EXCESSPRECISION OFF}
+
+{ TGradient }
+ const
+  k255 = 1/255;
+
+ function TColorGradient.ColorAt(x,y:single):cardinal;
+  begin
+   result:=MyColorF(alpha.ValueAt(x,y),red.valueAt(x,y),green.ValueAt(x,y),blue.ValueAt(x,y));
+  end;
+
+ procedure TColorGradient.Init(color1,color2:cardinal;angle,scale:single);
+  begin
+   alpha.Init(PARGBColor(@color1).a*k255,PARGBColor(@color2).a*k255,angle,scale);
+   red.Init(PARGBColor(@color1).r*k255,PARGBColor(@color2).r*k255,angle,scale);
+   green.Init(PARGBColor(@color1).g*k255,PARGBColor(@color2).g*k255,angle,scale);
+   blue.Init(PARGBColor(@color1).b*k255,PARGBColor(@color2).b*k255,angle,scale);
+  end;
+
+{ TMonoGradient }
+ procedure TMonoGradient.Init(v1,v2,angle,scale:single);
+  begin
+   base:=(v1+v2)/2;
+   dx:=(v2-v1)*cos(angle)/scale;
+   dy:=(v2-v1)*sin(angle)/scale;
+  end;
+
+ function TMonoGradient.ValueAt(x,y:single):single;
+  begin
+   result:=Clamp(base+(x*2-1)*dx+(y*2-1)*dy,0,1);
+  end;
 
 end.
