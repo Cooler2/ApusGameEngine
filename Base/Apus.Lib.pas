129459afe06565916383a6c760136c35e63fb64a
Cooler <ivan@apus-software.com>
2026-02-16 17:00:41 +0300

    Add Apus.Lib convenience re-export module
    Aggregates commonly used modules (Types, Classes, Conv, HashMaps, Strings)
    for convenient import. Reduces boilerplate in applications that need
    multiple base utilities.
    
    Note: String8Helper can't be re-exported - import Apus.Strings directly.

---
+++ Base/Apus.Lib.pas
@@ -0,0 +1,65 @@
+﻿// Convenience re-export module - single import for commonly used Apus Base functionality
+//
+// SCOPE: Aggregates frequently used modules (Conv, HashMaps, Log) for convenience.
+// Reduces boilerplate when you need multiple base modules. Use when building applications
+// that need general utilities without caring about fine-grained imports.
+//
+// Usage: uses Apus.Core, Apus.Lib;  // Core must be imported explicitly
+//
+// ADD HERE: Re-exports of stable, commonly used modules.
+// DON'T ADD: Specialized modules (Threads, Files), platform-specific code, experimental APIs.
+//
+// Note: Record helpers (String8Helper, String32Helper) can't be re-exported - import Apus.Strings directly.
+//
+// Copyright (C) Ivan Polyacov, ivan@apus-software.com
+// This file is licensed under the terms of BSD-3 license (see license.txt)
+// This file is a part of the Apus Base Library (http://apus-software.com/engine/#base)
+{$I defines.inc}
+unit Apus.Lib;
+interface
+uses
+  Apus.Types,
+  Apus.Classes,
+  Apus.Conv,
+  Apus.HashMaps,
+  Apus.Strings;
+
+type
+  // --- Apus.Types ---
+  TSplineFunc = Apus.Types.TSplineFunc;
+  TIntRange = Apus.Types.TIntRange;
+  TFloatRange = Apus.Types.TFloatRange;
+  TNameValue = Apus.Types.TNameValue;
+  TNameValueList = Apus.Types.TNameValueList;
+  TBuffer = Apus.Types.TBuffer;
+  TWriteBuffer = Apus.Types.TWriteBuffer;
+  PCriticalSection = Apus.Types.PCriticalSection;
+  TMyCriticalSection = Apus.Types.TMyCriticalSection;
+  {$IFDEF MSWINDOWS}
+  TSRWLock = Apus.Types.TSRWLock;
+  {$ENDIF}
+
+  // --- Apus.Classes ---
+  TObjectEx = Apus.Classes.TObjectEx;
+  TNamedObject = Apus.Classes.TNamedObject;
+  TNamedObjectClass = Apus.Classes.TNamedObjectClass;
+  TNamedObjects = Apus.Classes.TNamedObjects;
+  TBaseException = Apus.Classes.TBaseException;
+  EWarning = Apus.Classes.EWarning;
+  EError = Apus.Classes.EError;
+  EFatalError = Apus.Classes.EFatalError;
+
+  // --- Apus.Conv ---
+  Conv = Apus.Conv.Conv;
+
+  // --- Apus.Strings ---
+  UTF8 = Apus.Strings.UTF8;
+
+  // --- Apus.Types (generics) ---
+  TArray<T> = Apus.Types.TArray<T>;
+
+  // --- Apus.HashMaps ---
+  THashMap<T> = Apus.HashMaps.THashMap<T>;
+
+implementation
+end.
