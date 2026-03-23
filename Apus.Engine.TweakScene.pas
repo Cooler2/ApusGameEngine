527914844649ec207642dab60e7d42c4d262de40
Cooler <ivan@apus-software.com>
2026-03-23 19:15:42 +0300

    Fix API field names introduced by pre-staged batch: frameStartMs, mousePos
    Commit 849d795 accidentally included pre-staged changes that used
    window.frameStartTime and window.mouseX/mouseY (from engine5 naming)
    which don't exist in the current Window API. Revert to:
    - window.frameStartMs (DefaultStyle, TweakScene)
    - window.mousePos.x / window.mousePos.y (TweakScene)

--- Apus.Engine.TweakScene.pas
+++ Apus.Engine.TweakScene.pas
@@ -184,7 +184,7 @@ begin
  end;
 
  // Adjust vertical position
- if not keepPos then ui.position.y:=window.mouseY-ui.height/2;
+ if not keepPos then ui.position.y:=window.mousePos.y-ui.height/2;
  if ui.position.y+ui.height>window.renderHeight then ui.position.y:=window.renderHeight-ui.height;
  if ui.position.y<5 then ui.position.y:=5;
 end;
@@ -197,7 +197,7 @@ begin
  if st=TSceneStatus.ssActive then begin
   // Update UI Layout
   ui.Resize(round(200+window.renderWidth*0.1),-1);
-  ui.position.x:=window.mouseX-ui.width/2;
+  ui.position.x:=window.mousePos.x-ui.width/2;
   if ui.position.x<5 then ui.position.x:=5;
   if ui.position.x+ui.width>window.renderWidth-5 then ui.position.x:=window.renderWidth-5-ui.width;
   ui.height:=listBox.height+20;
@@ -335,7 +335,7 @@ begin
    vtBlue: c:=$FFA0A0F0;
   end;
   inc(xx,x1);
-  if focused then c:=ColorAlpha(c,0.9+0.2*sin(window.frameStartTime/50));
+  if focused then c:=ColorAlpha(c,0.9+0.2*sin(window.frameStartMs/50));
   for i:=-4 to 4 do
    draw.Line(xx+i,yy-j,xx+i,yy-abs(i),c-$101010*abs(i));
  end;
