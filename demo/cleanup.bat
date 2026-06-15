26dd9bf2693b96938eaae2aeeb64898a44e81ef0
Cooler <ivan@apus-software.com>
2026-06-15 20:39:25 +0300

    Fix cleanup script to keep OBJ resources

--- demo/cleanup.bat
+++ demo/cleanup.bat
@@ -9,9 +9,9 @@ rem   demo\cleanup.bat dry-run
 pushd "%~dp0.." || exit /b 1
 
 if /I "%~1"=="dry-run" (
-  git clean -ndX -- demo
+  git clean -ndX -- demo ":(exclude)demo/**/res/*.obj"
 ) else (
-  git clean -fdX -- demo
+  git clean -fdX -- demo ":(exclude)demo/**/res/*.obj"
 )
 
 set "ERR=%ERRORLEVEL%"
